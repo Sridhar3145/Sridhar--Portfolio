ec56e1c060f954152b8d082b656d5544e0ef727d
sridhar3145 <sridhar314507@gmail.com>
2025-03-18 11:39:43 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./Components/home/Home";
 import About from "./Components/about/About";
 import Skills from "./Components/skills/Skills";
 import Project from "./Components/project/Project";
 import Contact from "./Components/contact/Contact";
-
 import NavBar from "./Components/Navbar/NavBar";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // 2 sec wait and remove loading screen
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <Router>
-        <NavBar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      {loading ? (
+        // Loading screen
+        <div className="flex items-center justify-center h-screen text-5xl font-bold text-white">
+          Loading...
+        </div>
+      ) : (
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
