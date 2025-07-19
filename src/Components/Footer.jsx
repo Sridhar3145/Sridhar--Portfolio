a195c44443f7ec180f44f3dfecd71605abce67cc
sridhar3145 <sridhar314507@gmail.com>
2025-07-19 16:53:21 +0530

    change in code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="text-white flex justify-center items-center font-abc text-xl mb-10">
-        &copy; 2025 Sridhar{" "}
-        <span className="text-green-400 ml-2"> Portfolio</span> | All rights
-        reserved.
+      <div className="text-white flex flex-wrap justify-center items-center font-abc text-sm sm:text-base md:text-xl mb-10 px-4 text-center gap-1">
+        &copy; 2025 Sridhar
+        <span className="text-green-400">Portfolio</span>| All rights reserved.
       </div>
     </>
   );
